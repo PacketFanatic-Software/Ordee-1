efce17daa321a258c7ab9b0f1a70d0f1d701a9af
Daksha Hedge <dakshahegde5@gmail.com>
2020-10-17 18:33:54 +0530

    init

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import './Subtotal.css'
-import CurrencyFormat from "react-currency-format";
-import { useStateValue } from "./StateProvider";
+// import React from 'react'
+// import './Subtotal.css'
+// import CurrencyFormat from "react-currency-format";
+// import { useStateValue } from "./StateProvider";
 
-function Subtotal() {
-    return (
-        <div className="subtotal">
+// function Subtotal() {
+//     return (
+//         <div className="subtotal">
 
-            <CurrencyFormat
-            renderText={(value) => (
-                <>
-                <p>
-                    Subtotal ({basket.length} items): <strong>{` $
-                </p>
-                <small className="subtotal_gift">
-                    <input type="checkbox" /> This order contains a gift
-                </small>
-            )}
-            decimalScale={2}
-            {/* value={getBasketTotal(basket)} */}
-            displayType={"text"}
-            thousandSeparator={true}
-            prefix={"$"}
-            />
-            <button>Proceed to Checkout</button>          
-        </div>
-    )
-}
+//             <CurrencyFormat
+//             renderText={(value) => (
+//                 <>
+//                 <p>
+//                     Subtotal ({basket.length} items): <strong>{` $
+//                 </p>
+//                 <small className="subtotal_gift">
+//                     <input type="checkbox" /> This order contains a gift
+//                 </small>
+//             )}
+//             decimalScale={2}
+//             {/* value={getBasketTotal(basket)} */}
+//             displayType={"text"}
+//             thousandSeparator={true}
+//             prefix={"$"}
+//             />
+//             <button>Proceed to Checkout</button>          
+//         </div>
+//     )
+// }
 
-export default Subtotal;
+// export default Subtotal;
