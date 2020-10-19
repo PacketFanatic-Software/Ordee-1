79c52f8b99ca3320d4abcd75684c28704a2f3011
Daksha Hedge <dakshahegde5@gmail.com>
2020-10-20 00:44:47 +0530

    commitagain

--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -10,7 +10,8 @@ export const initialState = {
   user: null,
 };
 
-const reducer = (state, action) => {
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
+function reducer(state, action) {
     console.log(action);
     switch(action.type) {
         case 'Add_To_Basket':
@@ -19,6 +20,7 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item]
             };
+            
         case 'REMOVE_FROM_BASKET':
             // logic for removing from basket
             let newBasket = [...state.basket];
@@ -31,8 +33,6 @@ const reducer = (state, action) => {
                 console.warn(
                     'Cannot remove product (id: $(action.id) as it is not in the basket');
             }
-
-
             return {...state, basket: newBasket };
         default:
             return state; 
